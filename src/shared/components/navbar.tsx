dac8713ce902005e5b70c9593d85560a162cab7e
reddu25 <43440036+reddu25@users.noreply.github.com>
2020-01-31 17:04:36 +0100

    Implements navbar

---
+++ src/shared/components/navbar.tsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Menu } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+
+/**
+ * Navbar component.
+ */
+export class NavBar extends Component {
+    // State initialization.
+    state = { activeItem: 'home' };
+
+    render() {
+        return(
+            <div>
+                <Menu pointing>
+                    <Menu.Item as={ Link }
+                        name='home'
+                        to='/'
+                    />
+                    <Menu.Item as={ Link }
+                        name='register'
+                        to='/register'
+                    />
+                </Menu>
+            </div>
+        )
+    }
+}
