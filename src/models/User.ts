759dbd9e7714c3b1f6b4e7a5a166359bf4b237c4
reddu25 <43440036+reddu25@users.noreply.github.com>
2020-01-31 17:05:25 +0100

    ResponseType, User update, import semantic css

--- src/models/User.ts
+++ src/models/User.ts
@@ -3,18 +3,16 @@ import { Poll } from './Poll';
 export class User {
     id?: string | null;
     email: string;
-    name: string;
+    name?: string;
     password: string;
     polls?: Array<Poll>
 
     constructor(
         email: string,
-        name: string,
         password: string,
 
     ) {
         this.email = email;
-        this.name = name;
         this.password = password;
     }
 }
